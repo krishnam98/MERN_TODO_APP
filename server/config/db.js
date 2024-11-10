b11eb6c8d281aa8ce9941564419655f23f115387
Krishnam <sonikrishnam98@gmail.com>
2024-11-10 22:19:16 +0530

    Created function to connect MongoDB with out server

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected!");
+  } catch (error) {
+    console.log(error), process.exit(1);
+  }
+};
+
+module.exports = connectDB;
