6a6935344b36312049edad52a90f878f183ffae7
Krishnam <sonikrishnam98@gmail.com>
2024-11-21 19:52:26 +0530

    Created daySchema and created priority Attribute to task field

--- server/models/task.js
+++ server/models/task.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 
 const TaskSchema = mongoose.Schema({
   title: { type: String, require: true },
+  priority: { type: Boolean, require: true },
   completed: { type: Boolean, default: false },
 });
-module.exports = mongoose.model("Task", TaskSchema);
+
+const DaySchema = mongoose.Schema({
+  day: { type: String, require: true },
+  Story: { type: String, require: false },
+  task: [TaskSchema],
+});
+
+module.exports = mongoose.model("Day", DaySchema);
