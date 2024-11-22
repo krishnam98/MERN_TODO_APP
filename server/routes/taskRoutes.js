d89f25e0f0788ae6ecec5df7e325bf8b822d28d8
Krishnam <sonikrishnam98@gmail.com>
2024-11-22 10:16:20 +0530

    Created EndPoints  To add or Update tasks for specific day, get all days & delete a task for specific day

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const task = require("../models/task");
+const Day = require("../models/task");
 
 // Post Request
 
 router.post("/add", async (req, res) => {
   try {
-    const newtask = new task(req.body);
-    await newtask.save();
-    res.json(newtask);
+    const filter = { day: req.body.day };
+    const options = { upsert: false };
+    const updateTask = {
+      $push: {
+        task: req.body.task,
+      },
+    };
+    const result = await Day.updateOne(filter, updateTask, options);
+    const UpdatedDay = await Day.findOne({ day: req.body.day });
+    res.json(UpdatedDay);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -18,8 +25,32 @@ router.post("/add", async (req, res) => {
 
 router.get("/get", async (req, res) => {
   try {
-    const tasks = task.find();
-    res.json(tasks);
+    const allDays = await Day.find();
+    res.json(allDays);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// DELETE Request
+
+router.delete("/delete", async (req, res) => {
+  const day = req.body.day;
+  const taskID = req.body.task_id;
+  try {
+    const updatedDay = await Day.findOneAndUpdate(
+      { day },
+
+      /*in Array of days this query searches for task field in days
+       and furthur in task it searches for _id field*/
+      {
+        $pull: {
+          task: { _id: taskID },
+        },
+      },
+      { new: true } //return Updated Document
+    );
+    res.json(updatedDay);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
